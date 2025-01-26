12da86b14fbb2f490d8cd4790672b3d284976fab
Dovydas Musulas <dovydas.musulas@proton.me>
2025-01-26 23:33:54 +0100

    ex00

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dmusulas <dmusulas@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/26 22:44:24 by dmusulas          #+#    #+#             */
+/*   Updated: 2025/01/26 23:29:37 by dmusulas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main(int argc, char *argv[]){
+	if (argc == 1) {
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	}
+	if (argc > 1) {
+			for (int i = 1; i < argc; ++i) {
+				std::string s = argv[i];
+				std::transform(s.begin(), s.end(), s.begin(), ::toupper);
+				std::cout << s;
+			}
+			std::cout << std::endl;
+		}
+}
